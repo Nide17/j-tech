242f9f72fc2049a49b0f7ce5ea08f6e6a027b262
Nide17 <nidedrogba@gmail.com>
2022-03-15 18:05:35 +0200

    removed mui

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,7 @@
-import React from 'react';
-import { CacheProvider } from '@emotion/react';
-import { ThemeProvider, CssBaseline } from '@mui/material';
+import '../styles/globals.css'
 
-import createEmotionCache from '../utility/createEmotionCache';
-import lightTheme from '../styles/theme/lightTheme';
-import '../styles/globals.css';
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
 
-const clientSideEmotionCache = createEmotionCache();
-
-const MyApp = (props) => {
-  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
-
-  return (
-    <CacheProvider value={emotionCache}>
-      <ThemeProvider theme={lightTheme}>
-        <CssBaseline />
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </CacheProvider>
-  );
-};
-
-export default MyApp;
+export default MyApp
