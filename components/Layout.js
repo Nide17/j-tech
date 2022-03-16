910b22ff6924d8d3bee47c9170df8d3437629a97
Nide17 <nidedrogba@gmail.com>
2022-03-16 09:03:28 +0200

    navbar

--- components/Layout.js
+++ components/Layout.js
@@ -11,7 +11,7 @@ export default function Layout({ children }) {
             <Meta />
             <div className={styles.container}>
             <TopAlert />
-                {/* <Navbar /> */}
+                <Navbar />
                 {children}
                 {/* <Footer /> */}
             </div>
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,5 +1,6 @@
 import navbarStyles from '../styles/Navbar.module.css'
 import Link from 'next/link'
+import Image from 'next/image'
 
 export default function Navbar() {
 
@@ -8,15 +9,17 @@ export default function Navbar() {
 
             <Link href="/">
                 <a className={navbarStyles.brand}>
-                    <span className='ayi'>I</span><span className='pi'>P</span><span className='ani'>N</span>
+                    <span className='d-flex'>
+                    <Image src="/images/J-TECH.png" alt='placeholder' width={60} height={60} />
+                    </span>
                 </a>
             </Link>
 
             <ul className={navbarStyles.links}>
                 <li><Link href="/#home"><a>H<span>ome</span></a></Link></li>
-                <li><Link href="/#about"><a>A<span>bout</span></a></Link></li>
-                <li><Link href="/#projects"><a>P<span>rojects</span></a></Link></li>
-                <li><Link href="/#contact"><a>C<span>ontact</span></a></Link></li>
+                <li><Link href="/#services"><a>S<span>ervices</span></a></Link></li>
+                <li><Link href="/#about"><a>A<span>bout us</span></a></Link></li>
+                <li><Link href="/#contact"><a>C<span>ontact us</span></a></Link></li>
             </ul>
         </nav>
     )
