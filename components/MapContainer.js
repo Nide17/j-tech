252644c064ed9f6de2c969891c398cc56d065e8a
Nide17 <nidedrogba@gmail.com>
2022-03-20 13:21:20 +0200

    map border

--- components/MapContainer.js
+++ components/MapContainer.js
@@ -2,6 +2,8 @@ import { Map, GoogleApiWrapper, Marker } from 'google-maps-react'
 import React from 'react';
 
 const mapStyles = {
+  width: "100%",
+  height: "100%"
 };
 
 const MapContainer = (props) => {
